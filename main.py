ad5a644506717b9e30a75e7e15b0ed56e6dd475e
vaibhavw@dynamischit.com <vaibhavw@dynamischit.com>
2025-05-17 22:55:26 +0530

    add docs in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI # type: ignore
 from fastapi.middleware.cors import CORSMiddleware # type: ignore
 from fastapi.staticfiles import StaticFiles # type: ignore
+from fastapi.responses import RedirectResponse
 import auth
 from donations import router as donations_router
 from volunteers import router as volunteers_router
@@ -12,14 +13,14 @@ app.include_router(volunteers_router)
 app.include_router(contact_router)
 
 # Update CORS settings
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Must be explicit for credentials
-    allow_credentials=True,  # Allow cookies
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:3000"],  # Must be explicit for credentials
+#     allow_credentials=True,  # Allow cookies
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-@app.get("/")
-def root():
-    return {"message": "Backend running"}
+@app.get("/", include_in_schema=False)
+def index():
+    return RedirectResponse("/docs",status_code=308)
